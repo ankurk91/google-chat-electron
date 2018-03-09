6d1f89502ff49834a0e86455d98ff01741791eff
robyf <rob@robyf.net>
2018-03-09 17:54:15 +0200

    Application working

---
+++ main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const electron = require('electron');
+var app = electron.app;  // Module to control application life.
+var WrappedWindow = require('./wrappedWindow');
+
+// Report crashes to our server.
+//electron.crashReporter.start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  mainWindow = WrappedWindow({
+    name: 'Google Hangouts Chat',
+    url: 'https://chat.google.com/',
+    openLocally: false
+  });
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
---
+++ wrappedWindow.js
@@ -0,0 +1,109 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto');
+const electron = require('electron');
+var app = electron.app; // Module to control application life.
+var BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+var localShortcut = require('electron-localshortcut'); // Module to register keyboard shortcuts
+
+module.exports = function createWrappedWindow(opts) {
+  // Thanks imskull! (https://github.com/atom/electron/issues/526#issuecomment-132942967)
+  // Try to load saved window bounds
+  var initPath = path.join(app.getPath("userData"), 'init.json');
+  var data;
+  try {
+    data = JSON.parse(fs.readFileSync(initPath, 'utf8'));
+  }
+  catch(e) { }
+
+  var sha = crypto.createHash('sha256');
+  sha.update(opts.name);
+  var hash = sha.digest('hex');
+
+  const iconPath = path.join(__dirname, 'assets/icon/icon.png');
+
+  // Create the browser window.
+  var windowOpts = (data && data[hash] && data[hash].bounds) ? data[hash].bounds : { width: 800, height: 600 };
+  windowOpts['auto-hide-menu-bar'] = true;
+  windowOpts['web-preferences'] = { 'node-integration': false };
+  windowOpts['icon'] = iconPath;
+  var window = new BrowserWindow(windowOpts);
+  window.setMenu(null);
+
+  if (data && data[hash] && data[hash].shouldBeMaximized) {
+    window.maximize();
+  }
+
+  // and load the url ;)
+  window.loadURL(opts.url);
+
+  // Register common navigation shortcuts
+  function goBack() {
+    if (window.webContents.canGoBack()) {
+      window.webContents.goBack();
+    }
+  }
+  function goForward() {
+    if (window.webContents.canGoForward()) {
+      window.webContents.goForward();
+    }
+  }
+  localShortcut.register(window, 'Alt+Left', goBack);
+  localShortcut.register(window, 'Alt+Right', goForward);
+  localShortcut.register(window, 'F5', window.webContents.reload);
+
+  // Open EXTERNAL LINKS in the default browser
+  // Example: electron-wrap messenger.com
+  // Links on messenger.com that link to messenger.com will be followed in-app
+  // Links on messenger.com that link elsewhere (e.g., imgur.com) will be opened externally
+
+  // http://stackoverflow.com/a/23945027/5136076
+  function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+      domain = url.split('/')[2];
+    }
+    else {
+      domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+  }
+  var handleRedirect = function(e, url) {
+    e.preventDefault();
+    if (!opts.openLocally && extractDomain(url) !== extractDomain(window.webContents.getURL())) {
+      require('electron').shell.openExternal(url);
+    } else {
+      var nested = createWrappedWindow({
+        name: opts.name,
+        url: url,
+        openLocally: opts.openLocally
+      });
+      nested.on('closed', function() {
+        nested = null;
+      });
+    }
+  };
+
+  window.webContents.on('will-navigate', handleRedirect);
+  window.webContents.on('new-window', handleRedirect);
+
+  // Save the window bounds on close
+  window.on('close', function() {
+    var maximized = window.isMaximized();
+    var newData = data || {};
+    newData[hash] = { bounds: window.getBounds() };
+    if (window.isMaximized()) {
+      newData[hash].shouldBeMaximized = true;
+    }
+    fs.writeFileSync(initPath, JSON.stringify(newData));
+  });
+
+  return window;
+};
