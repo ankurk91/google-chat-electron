9d20c762657dc222f7868833083094d144ec4d9d
ankurk91 <pro.ankurk1@gmail.com>
2020-12-05 13:52:14 +0530

    Check for updates every week

--- src/js/features/updateNotification.js
+++ src/js/features/updateNotification.js
@@ -1,7 +1,15 @@
-const {setUpdateNotification} = require('electron-update-notifier');
+const {setUpdateNotification, checkForUpdates} = require('electron-update-notifier');
+
+let interval = null;
 
 module.exports = () => {
   setTimeout(() => {
     setUpdateNotification();
   }, 5000)
+
+  interval = setInterval(() => {
+    checkForUpdates()
+  }, 1000 * 60 * 60 * 24 * 7);
+
+  return interval
 }
