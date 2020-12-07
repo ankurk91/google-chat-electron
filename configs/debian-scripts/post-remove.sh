55bada40739eaaad304e6c94753d8aaedc15f055
ankurk91 <pro.ankurk1@gmail.com>
2020-12-07 13:30:14 +0530

    Refactor post removal script

--- configs/debian-scripts/post-remove.sh
+++ configs/debian-scripts/post-remove.sh
@@ -5,21 +5,21 @@ IFS=$'\n\t'
 echo "Starting post removal script ..."
 
 # Variables
-USER_HOME=~
+USER_HOME_DIR=~
 APP_FOLDER_NAME=google-chat-electron
 
 # https://stackoverflow.com/questions/7358611/get-users-home-directory-when-they-run-a-script-as-root
 
 if [[ $EUID == 0 ]]; then
-    USER_HOME=$(eval echo ~${SUDO_USER})
+    USER_HOME_DIR=$(eval echo ~${SUDO_USER})
 fi
 
-echo "Detected your home directory as - $USER_HOME"
+echo "Detected your home directory as - $USER_HOME_DIR"
 
 echo "Removing autostart config file"
-rm -f "$USER_HOME/.config/autostart/$APP_FOLDER_NAME.desktop"
+rm -f "$USER_HOME_DIR/.config/autostart/$APP_FOLDER_NAME.desktop"
 
 echo "Removing app cache folder"
-rm -rf "$USER_HOME/.config/$APP_FOLDER_NAME/"
+rm -rf "$USER_HOME_DIR/.config/$APP_FOLDER_NAME/"
 
 echo "Finished post removal script!"
