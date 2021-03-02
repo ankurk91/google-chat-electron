71116b99c8fd66bc2e97367823bfdc80537045d3
ankurk91 <pro.ankurk1@gmail.com>
2021-03-02 10:43:15 +0530

    Limit the auto online attempts checks

--- src/offline/index.ts
+++ src/offline/index.ts
@@ -1,14 +1,22 @@
 ((window: Window) => {
   const btn: HTMLButtonElement = <HTMLButtonElement>window.document.getElementById('retry-btn')
+  const maxAutoAttemptCount = 100;
+  let attemptCount = 0
+  let interval: NodeJS.Timeout;
 
   const checkIsOnline = () => {
+    if (attemptCount > maxAutoAttemptCount) {
+      clearInterval(interval);
+    }
+
     btn.disabled = true;
     btn.innerText = 'Checking...';
 
     window.dispatchEvent(new Event('gchat:checkIfOnline'));
+    attemptCount++
   }
 
   btn.addEventListener('click', checkIsOnline);
-  setInterval(checkIsOnline, 1000 * 60)
+  interval = setInterval(checkIsOnline, 1000 * 60)
 })(window)
 
