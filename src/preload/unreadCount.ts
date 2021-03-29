8cb91e927866a5fc7ff9d4f10629a3cb5f5edbeb
ankurk91 <pro.ankurk1@gmail.com>
2021-03-29 11:38:01 +0530

    Fix: unread badge count for new UI

--- src/preload/unreadCount.ts
+++ src/preload/unreadCount.ts
@@ -1,31 +1,23 @@
-// https://github.com/getferdi/recipes/blob/822c5680c0/recipes/hangoutschat/webview.js
 import {ipcRenderer} from 'electron';
 
-// class corresponding to the mute icon
-const muteSelector = ".DQy0Rb";
-
-// class corresponding to the bold text that is visible for all messages
-const allMessageSelector = ".IL9EXe.PL5Wwe.dHI9xe.H7du2";
-
-const isMuted = (node: any): Boolean => {
-  const closestItem = node.closest('[role="listitem"]')
-  if (closestItem) {
-    return !!closestItem.querySelector(muteSelector);
-  }
-  return true;
-}
+const targetSelectors = [
+  'div[data-tooltip="Chat"][role="group"]',
+  'div[data-tooltip="Rooms"][role="group"]'
+];
 
 const getMessageCount = (): Number => {
   let counter = 0;
 
-  document.querySelectorAll(allMessageSelector).forEach((node) => {
-    if (!isMuted(node)) {
-      counter += 1;
-    }
-  });
+  document.body.querySelectorAll(targetSelectors.join(','))
+    .forEach((target) => {
+      const span = target.querySelector('span[role="heading"]')?.nextElementSibling
+      if (span) {
+        counter = counter + Number(span.textContent)
+      }
+    })
 
   return counter
-};
+}
 
 const emitCount = () => {
   ipcRenderer.send('unreadCount', getMessageCount())
