f21e72fae83b31b361d8efa4c00d5b52c2eb53c7
ankurk91 <pro.ankurk1@gmail.com>
2021-05-04 08:53:30 +0530

    Refactor

--- src/urls.ts
+++ src/urls.ts
@@ -1,4 +1,6 @@
+const appUrl = 'https://mail.google.com/chat/u/0';
+
 export default Object.freeze({
-  appUrl: 'https://mail.google.com/chat/u/0',
-  logoutUrl: 'https://www.google.com/accounts/Logout?continue=https://mail.google.com/chat/u/0'
+  appUrl,
+  logoutUrl: 'https://www.google.com/accounts/Logout?continue=' + appUrl
 })
